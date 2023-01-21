9585bbde4a8fc8f3fa439dfe200140d32af4cc06
ArefBr <108584817+ArefBr@users.noreply.github.com>
2023-01-21 19:56:48 +0330

    added images & data tags

--- script.js
+++ script.js
@@ -20,4 +20,12 @@ $(document).ready(function() {
       });
     });
     
-  
+    //TODO: Remove Item: You can add a "remove" button next to each item in the cart, which will allow the user to remove an item from the cart if they change their mind.
+
+    //TODO: Update Quantity: You can add an input field next to each item in the cart to allow the user to update the quantity of that item in the cart.
+    
+    //TODO: Clear Cart: You can add a "Clear Cart" button to the cart section that will allow the user to easily remove all items from the cart.
+    
+    //TODO: Persistence: You can use the LocalStorage object to persist the cart data even when the user closes the browser or navigates to a different page. This way, when the user returns to the website, their cart will still contain the items they previously added.
+    
+    //TODO: LocalStorage : You can add functionality to retrieve the cart data from LocalStorage when the page is loaded, and show the cart items and total cost in the cart section accordingly.
