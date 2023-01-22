cf9f2cb8ce65b3b7f7867049fcc49ae9b430de94
ArefBr <108584817+ArefBr@users.noreply.github.com>
2023-01-22 12:55:33 +0330

    .

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// $(document).ready(function() {
-//     $('.add-to-cart').click(function() {
-//         let productName = $(this).closest('.card').find('.card-title').text();
-//         let productPrice = $(this).closest('.card').find('.card-text').text();
-//         let product = { name: productName, price: productPrice };
-    
-//         // Check if cart already exists in LocalStorage
-//         if (localStorage.getItem("cart") === null) {
-//           // If not, create a new cart and add the first item
-//           let cart = [];
-//           cart.push(product);
-//           localStorage.setItem("cart", JSON.stringify(cart));
-//         } else {
-//           // If yes, retrieve the cart and add the new item
-//           let cart = JSON.parse(localStorage.getItem("cart"));
-//           cart.push(product);
-//           localStorage.setItem("cart", JSON.stringify(cart));
-//         }
-//         alert('Product added to cart!');
-//       });
-//     });
-
 // Get the "add to cart" buttons, cart section, and total cost span
 const addToCartButtons = document.querySelectorAll('.add-to-cart');
 const cart = document.querySelector('.cart');
